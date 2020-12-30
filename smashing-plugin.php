dee53955edbc8a38fdfd9e492d037c492c8cf9a5
mconslayer <mconslayer@gmail.com>
2020-12-30 21:05:30 +0700

    Added

---
+++ smashing-plugin.php
@@ -0,0 +1,18 @@
+<?php
+/*
+	Plugin Name: MS Woo
+	Description: This is for updating your Wordpress plugin.
+	Version: 1.0.0
+	Author: Stepclick
+*/
+if( ! class_exists( 'Smashing_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
+
+$updater = new Smashing_Updater( __FILE__ );
+$updater->set_username( 'mconslayer' );
+$updater->set_repository( 'mswoo' );
+/*
+	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+*/
+$updater->initialize();
